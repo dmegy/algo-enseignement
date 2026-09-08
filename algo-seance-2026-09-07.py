a677afb5d3c42fce37b8ec0f8b8823d9b0ad2be8
Damien Mégy <damien.megy@univ-lorraine.fr>
2026-09-08 18:27:04 +0200

    typos et petites remarques

--- algo-seance-2026-09-07.py
+++ algo-seance-2026-09-07.py
@@ -1,26 +1,37 @@
+# ===========================
 # Implémentation en Python de quelques algorithmes vus lors de la séance du 7 septembre 2026 (1ère séance de l'année)
+# ===========================
+
 
 # En Python, on définit une fonction avec le mot-clé "def" (au lieu de "fonction" en pseudo-code), puis deux points ":"
 
-# Rappel : en Pthon, les listes sont numérotées à partir de l'indice zéro. Le premier élément d 'une liste L est L[0]
+# === Rappel : en Python, les listes sont numérotées à partir de l'indice zéro. Le premier élément d'une liste L est L[0]
 
-# Remarque : déclarer les types n'est pas obligatoire en Python, mais conseillé. J'essaierai de toujours le faire.
-# Les types de base sont int pour les entiers, flot pour les flottants (ce qui émule les réels, mais pas toujours bien), bool, list et quelques autres qu el'on verra par la suite
+# === Remarque : déclarer les types n'est pas obligatoire en Python, mais conseillé. J'essaierai de toujours le faire.
+# Les types de base sont `int` pour les entiers, `float` pour les flottants (ce qui émule les réels, mais pas toujours bien), `bool`, `list` et quelques autres que l'on verra par la suite
+
+# === Attention : les fonctions suivantes n'effectuent pas de tests sur les variables d'entrée.
+# Par exemple, les fonctions pour Fibonacci(n) ne vérifient pas que "n" est bien un entier, et qu'il est positif.
+# La raison est que ceci n'est pas un cours de développement informatique, c'est un cours d'algo.
+# Si on voulait avoir un code plus sûr pour une utilisation réelle, on pourrait mettre des "assert" dans les fonctions, ou tout autre mécanisme de gestion des erreurs. Ce n'est pas le but ici.
 
 
-def sommeRecursif(L:list[float])-> float:
+def sommeRecursif(L:list[float]) -> float:
 	if L == []:
 		return 0
 	return L[0] + sommeRecursif(L[1:])
 
-# En Python, L[1:] est la liste extraite de L en ne prenant que les éléments à partir de la position 1 (donc en ne prenant pas l'élément en position zéro)
-# De même, L[3:] est la sous-liste obtenue en ne prenant que les termes en position 3 et plus (donc à partir du 4ème élément de la liste, celui en position 3.)
+# Rq : en Python, L[1:] est la liste extraite de L en ne prenant que les éléments à partir de l'indice 1 (donc en ne prenant pas l'élément en indice zéro)
+# De même, L[3:] est la sous-liste obtenue en ne prenant que les termes à l'indice 3 et plus (donc à partir du 4ème élément de la liste, celui en indice 3.)
+
+# Rq d'optimisation en Python (un peu plus hors-sujet pour ce qui nous intéresse) : `L[1:]` crée en fait une nouvelle liste, donc ceci est "lent"
 
 # =====
-# test:
+# Tests
 # =====
 
-# à chaque fois, si l'égalité n'est pas vérifiée, Python renverra une erreur.
+# Explication de la syntaxe de "assert" : 
+# À chaque fois, si la condition après le `assert` n'est pas vérifiée, Python renverra une erreur.
 # Si "rien ne se passe", cela signifie que les tests ont réussi et que tout va bien
 
 assert sommeRecursif([]) == 0
@@ -32,7 +43,7 @@ print("Tests réussis pour la somme en récursif !")
 
 
 
-# La somme en itératif, aec une boucle for :
+# La somme en itératif, avec une boucle for :
 
 def sommeIteratif(L: list[float]) -> float:
 	s = 0
@@ -41,7 +52,9 @@ def sommeIteratif(L: list[float]) -> float:
 		s = s + L[k]
 	return s
 
-# les mêmes tests : 
+# Rq : on écrit les boucles "de base" : on n'utilisera pas les raccourcis Python comme "for x in L", qui n'existent pas forcément dans d'autres langages.
+
+# Les mêmes tests : 
 
 assert sommeIteratif([]) == 0
 assert sommeIteratif([0]) == 0
@@ -49,6 +62,11 @@ assert sommeIteratif([3.14]) == 3.14
 assert sommeIteratif([0,-2,0,2,0]) == 0
 assert sommeIteratif([0,1,0,-3,57.6,0,0]) == 55.6
 print("Tests réussis pour la somme en itératif !")
+
+# === Attention aux flottants !
+# Ne **PAS** tester si sommeIteratif([0.1,0.2]) == 0.3 !!! 
+# Il y aurait une erreur due non pas à une erreur dans l'algo de somme, mais due aux approximations sur les flottants.
+
 
 # ================
 # Fibonacci
@@ -76,7 +94,7 @@ def FibonacciIteratif1(n: int) -> int:
 	return F[n]
 
 
-# l'implémentation suivant n'est pas plus rapide, mais elle est plus économe en mémoire car elle ne garde pas en mémoire tous les nombres de Fibonacci, juste les deux derniers à chaque étape
+# l'implémentation suivante n'est pas plus rapide, mais elle est plus économe en mémoire car elle ne garde pas en mémoire tous les nombres de Fibonacci, juste les deux derniers à chaque étape
 def FibonacciIteratif2(n: int) -> int:
 	if n <= 1:
 		return n
@@ -85,17 +103,20 @@ def FibonacciIteratif2(n: int) -> int:
 		a, b = b, a+b
 	return b
 
-# test : 
+
+# Affichage de quelques valeurs :  
 for k in range(100):
 	print(FibonacciIteratif2(k))
 
-# ceci devrait donner les 100 nombres (jusqu'à F_{99}, donc) de façon quasi-immédiate. (0.1s sur mon ordi qui date de 2020)
-# n'essayez surtout pas ceci avec Fibonacci récursif, vous planterez vraisemblablement votre machine. Même avec 40 au lieu de 100, l'algorithme récursif a du mal.
+# Ceci devrait donner les 100 nombres (jusqu'à F_{99}, donc) de façon quasi-immédiate. (0,1s sur mon ordi qui date de 2020)
+# N'essayez surtout pas ceci avec Fibonacci récursif, vous planterez vraisemblablement votre machine.
+# Même avec 40 au lieu de 100, l'algorithme récursif va effectuer des millions d'appels récursif. 
+# Exercice : combien de millions ?
 
 # =====================
 # Suggestion de travail sur le contenu de ce document : 
 # - chronométrez la vitesse de ces algorithmes
-# - tracez les graphes de la vitesse d'execution de Finonacci(n) en fonction de n : est-ce que ça semble linéaire ? Pour lesquels de ces algorithmes ?
+# - tracez les graphes de la vitesse d'exécution de Fibonacci(n) en fonction de n : est-ce que ça semble linéaire ? Pour lesquels de ces algorithmes ?
 
 # N'oubliez pas non plus de regarder les exercices de la feuille de TD distribuée.
 
